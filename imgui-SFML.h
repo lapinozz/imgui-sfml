149ce573729422d9dc72b3abfdbfaac2ab7cda50
lapinozz <monstrefou@gmail.com>
2026-08-21 00:00:57 -0400

    Remove the declaration of function that was deleted

--- imgui-SFML.h
+++ imgui-SFML.h
@@ -35,7 +35,6 @@ IMGUI_SFML_API void ProcessEvent(const sf::Window& window, const sf::Event& even
 
 IMGUI_SFML_API void Update(sf::RenderWindow& window, sf::Time dt);
 IMGUI_SFML_API void Update(sf::Window& window, sf::RenderTarget& target, sf::Time dt);
-IMGUI_SFML_API void Update(const sf::Vector2i& mousePos, const sf::Vector2f& displaySize, sf::Time dt);
 
 IMGUI_SFML_API void Render(sf::RenderWindow& window);
 IMGUI_SFML_API void Render(sf::RenderTarget& target);
